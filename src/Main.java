a793f63a9616c52d0209f595bcc4797b9497254e
rubenyh <rubenyanezhernandez@gmail.com>
2026-04-28 17:56:25 -0700

    refactor: divide main into functions for better testing

--- src/Main.java
+++ src/Main.java
@@ -11,28 +11,48 @@ import simulacion.Simulador;
 
 public class Main {
   public static void main(String[] args) throws Exception {
-    String ruta = "../casos_de_prueba/caso1_fcfs_convoy.txt";
-    Calendarizador algoritmo = new CalendarizadorFCFS();
-    //Calendarizador algoritmo = new CalendarizadorRR(2); // Quantum de 2
+    String ruta = "./casos_de_prueba/caso1_fcfs_convoy.txt";
 
-    List<PCB> procesos = new ArrayList<>();
-    BufferedReader br = new BufferedReader(new FileReader(ruta));
-    String linea;
-    while ((linea = br.readLine()) != null) {
-      if (linea.isBlank() || linea.startsWith("#"))
-        continue;
-      String[] p = linea.split(",");
-      int pid = Integer.parseInt(p[0].trim());
-      String nombre = p[1].trim();
-      int llegada = Integer.parseInt(p[2].trim());
-      int rafaga = Integer.parseInt(p[3].trim());
-      int prioridad = Integer.parseInt(p[4].trim());
-      procesos.add(new PCB(pid, nombre, Estado.NUEVO, prioridad, llegada, rafaga));
+    List<Calendarizador> algoritmos = List.of(
+      new CalendarizadorFCFS()
+      // new CalendarizadorSJF(),
+      // new CalendarizadorRR(2), // Quantum de 2
+      // new CalendarizadorPrioridades()
+    );
+
+    for (Calendarizador alg : algoritmos) {
+      simular(ruta, alg, 0);
+      System.out.println();
     }
-    br.close();
+  }
 
-    Simulador simulador = new Simulador(algoritmo, procesos, 0);
+  private static ResultadoSimulacion simular(String ruta, Calendarizador algoritmo, int intervalAging) throws Exception {
+    List<PCB> procesos = cargarProcesos(ruta); 
+    Simulador simulador = new Simulador(algoritmo, procesos, intervalAging);
+
     ResultadoSimulacion resultado = simulador.ejecutar();
     resultado.imprimir();
+
+    return resultado;
+  }
+  private static List<PCB> cargarProcesos(String ruta) throws Exception {
+    List<PCB> procesos = new ArrayList<>();
+    try (BufferedReader br = new BufferedReader(new FileReader(ruta))) {
+      String linea;
+      while ((linea = br.readLine()) != null) {
+        if (linea.isBlank() || linea.startsWith("#")) 
+          continue;
+
+        String[] p = linea.split(",");
+        int pid = Integer.parseInt(p[0].trim());
+        String nombre = p[1].trim();
+        int llegada = Integer.parseInt(p[2].trim());
+        int rafaga = Integer.parseInt(p[3].trim());
+        int prioridad = Integer.parseInt(p[4].trim());
+
+        procesos.add(new PCB(pid, nombre, Estado.NUEVO, prioridad, llegada, rafaga));
+      }
+    }
+    return procesos;
   }
 }
